0497c26ad3c78d1eacb561d1f60695de18ba48fa
harshaal06 <harshalaglawe06@gmail.com>
2024-07-22 23:51:37 +0530

    main img add

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -87,6 +87,7 @@ function Dashboard() {
       setAmount('')
       setType('')
       setCategory('')
+      setDescription('')
 
       loadTransactions()
     }
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -7,6 +7,8 @@ import { motion } from "framer-motion"
 import { MyContext } from '../components/MyContext';
 import toast from 'react-hot-toast';
 import axios from 'axios';
+import MainImg from './lap.png';
+import MainImgS from './mob.jpg';
 
 const motionAni = (time) =>({
     hidden:{y:100,opacity:0},
@@ -34,9 +36,9 @@ function Home() {
         const data = JSON.parse(localStorage.getItem('user'));
         setUser(data);
     }
-    useEffect(() => {
-        loadUser();
-    }, [])
+    // useEffect(() => {
+    //     loadUser();
+    // }, [])
 
     const [showLogin, setShowLogin] = useState(false);
 
@@ -70,12 +72,16 @@ function Home() {
                 </div>
 
             </div>
-            <div className='absolute top-1/3 left-1/2 w-4/5 -translate-x-[50%] -translate-y-[50%] text-center'>
+            <div className='absolute md:mt-10 top-1/2 left-1/2 w-4/5 -translate-x-[50%] -translate-y-[50%] text-center'>
                 <motion.h1 variants={motionAni(0.2)} initial="hidden" animate='visible' className='text-5xl md:text-[75px] leading-none tracking-tighter font-semibold text-blck'>Track, manage, save, thrive</motion.h1>
-                <motion.h2 variants={motionAni(0.4)} initial="hidden" animate='visible' className='mt-3 text-center opacity-85 text-xl text-[27px] font-semibold leading-6 tracking-normal text-black'>Transform your financial habits<br />with ExpenseEase intuitive expense tracker.</motion.h2>
-                <motion.button variants={motionAni(0.5)} initial="hidden" animate='visible' className='mt-8 border-2 border-green-700 text-zinc-300 py-2 px-5 font-semibold rounded-lg hover:bg-[#343942]' 
+                <motion.h2 variants={motionAni(0.4)} initial="hidden" animate='visible' className='mt-2 text-center opacity-85 text-xl text-[27px] font-semibold leading-6 tracking-normal text-black'>Transform your financial habits<br />with ExpenseEase intuitive expense tracker.</motion.h2>
+                <motion.button variants={motionAni(0.5)} initial="hidden" animate='visible' className='mt-6 border-2 border-green-700 text-zinc-300 py-2 px-5 font-semibold rounded-lg hover:bg-[#343942]' 
                     onClick={() => !user ? setShowLogin(true) : navigate('/dashboard')}>Connect with ExpenseEase <GrFormNextLink className='inline' size="1.5em"/>
                 </motion.button>
+                <div className='relative mt-5 md:mt-2 mx-auto md:w-2/3'>
+                    <img src={MainImg} className='md:w-11/12'/>
+                    <img src={MainImgS} className='w-[22%] absolute end-0 md:end-3 top-16 md:top-24 rounded-xl'/>
+                </div>
             </div>
             {showLogin && <Login onClose={() => { setShowLogin(false) }} />}
         </div>
