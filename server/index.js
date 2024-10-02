3c0b18645d185e7fcf05ddf8837c7813000adcad
harshaal06 <harshalaglawe06@gmail.com>
2024-10-02 20:10:29 +0530

    .env add

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import { verifyToken } from './utils/verifyUser.js';
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'https://expenseease-app.vercel.app', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_SIDE_URI, credentials: true }));
 
 
 const connectDB = async () =>{
