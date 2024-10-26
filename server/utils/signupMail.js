36bd5b589923e159d769b81f41fc4db59f068c03
harshaal06 <harshalaglawe06@gmail.com>
2024-10-26 17:10:33 +0530

    small change

--- server/utils/signupMail.js
+++ server/utils/signupMail.js
@@ -13,7 +13,7 @@ function generateEmailTemplate(userName) {
         <title>Custom Email</title>
         <style>
           body { font-family: Arial, sans-serif; line-height: 1.6; color: #333; }
-          .container { max-width: 700px; margin: 0 auto; padding: 20px; background-color: #f4f4f4;}
+          .container { max-width: 700px; margin: 0 auto; padding: 10px; background-color: #f4f4f4;}
           .header {  padding-top: 10px; text-align: center; }
           .content { padding: 0 20px;}
         //   .message { font-size: 1.125em; }
