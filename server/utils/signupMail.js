d7f3087688cf772e20bb980792f008cc437acf9b
harshaal06 <harshalaglawe06@gmail.com>
2024-12-30 14:54:31 +0530

    logo add

--- server/utils/signupMail.js
+++ server/utils/signupMail.js
@@ -23,7 +23,7 @@ function generateEmailTemplate(userName) {
       <body>
         <div class="container">
           <div class="header">
-            <img src="#" alt="ExpenseEase" style="width: 80px;">
+            <img src="https://ibb.co/JqP6hPr" alt="ExpenseEase" style="width: 80px;">
           </div>
           <div class="content">
             <h1>Hi ${userName},</h1>
